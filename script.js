f0534b4c6e45661eb6e15abe0731dddae21387f6
rustamerk <rustam312@yandex.ru>
2020-03-31 11:05:39 +0300

    Кофемашина работает

--- script.js
+++ script.js
@@ -183,7 +183,36 @@ let changeBtn = document.querySelector(".change");
 changeBtn.onclick = takeChange;
 
 function takeChange() {
-  tossCoin("10");
+  if (balance.value <= 0) {
+    changeBtn.onclick = takeChange;
+    return;
+  }
+  changeBtn.onclick = null;
+  if (balance.value - 10 >= 0) {
+    setTimeout(() => {
+    tossCoin("10");
+    balance.value -= 10;
+    return takeChange();
+    },300)
+  } else if (balance.value - 5 >= 0) {
+    setTimeout(() => {
+    tossCoin("5");
+    balance.value -= 5;
+    return takeChange();
+    },300)
+  } else if (balance.value - 2 >= 0) {
+    setTimeout(() => {
+    tossCoin("2");
+    balance.value -= 2;
+    return takeChange();
+    },300)
+  } else if (balance.value - 1 >= 0) {
+    setTimeout(() => {
+    tossCoin("1");
+    balance.value -= 1;
+    return takeChange();
+    },300)
+  }
 }
 
 function tossCoin(cost) {
@@ -218,6 +247,7 @@ function tossCoin(cost) {
   coin.style.cutsor = "pointer";
   coin.style.display = "inline-block";
   coin.style.position = "absolute";
+  coin.style.userSelect = "none";
   
   changeContainer.append(coin); // прикрепить после /*changeContainer.prepend(coin); //Прикрепить до*/
   //append это внутренее наполнение контейнера; prepend - в начало
@@ -228,7 +258,12 @@ function tossCoin(cost) {
   coin.style.left = Math.round(Math.random() * (changeContainerCoords.width - 53)) + "px";
   
   coin.onclick = () => coin.remove();
+  
+   let coinSound = new Audio("sound/coindrop.mp3")
+    //coinSound.src = "sound/coindrop.mp3"
+    coinSound.play();
 }
+
 
 
 
